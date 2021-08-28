829f6409f7b6576cf0ae574b88a0d0ff6adb5a5a
MarecikM <magdalenaa.marecik@gmail.com>
2021-08-28 18:27:26 +0200

    final lines added

--- scraper.py
+++ scraper.py
@@ -1,28 +1,31 @@
 from price_parser import Price
 from bs4 import BeautifulSoup
+from datetime import datetime
+from time import sleep
 import requests
 import smtplib
 import json
 
+SEC_INTERVAL = 600
+GMAIL_USER = 'your_mail_address@gmail.com'
+GMAIL_PASSWORD = 'your_mail_password'
+SENT_TO = 'target_mail_address@gmail.com'
+SUBJECT = 'Price drop noticed!'
+BODY = 'Price of one of observed products has dropped.'
 
-def send_mail(gmail_user, gmail_password, sent_to, subject, body):
+def send_mail(gmail_user = GMAIL_USER, password = GMAIL_PASSWORD, sent_to = SENT_TO, subject = SUBJECT, body = BODY):
 
-    sent_from = gmail_user
-
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (sent_from, sent_to, subject, body)
+    email_text = 'From: %s\nTo: %s\nSubject: %s\n\n%s' % (gmail_user, sent_to, subject, body)
 
     try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
-        server.login(gmail_user, gmail_password)
-        server.sendmail(sent_from, sent_to, email_text)
-        server.close()
+        server.starttls()
+        server.ehlo()
+
+        server.login(gmail_user, password)
+        server.sendmail(gmail_user, sent_to, email_text)
+        server.quit()
 
     except Exception as e:
         print('Oops! Something went wrong sending an e-mail: ', e)
@@ -52,7 +55,7 @@ def price_scraper(products_path, supported_pages_path):
     for product in products_list['products']:
         url = product['url']
         domain = url.split('/')[2]
-        price = product['price']
+        price = float(product['price'])
 
 
         page_included = 0
@@ -70,7 +73,7 @@ def price_scraper(products_path, supported_pages_path):
 
             if response == 'Y' or 'y':
 
-                tag_name = input('Enter a html tag on %s page that contains a price of product.\n>' % domain)
+                tag_name = input('Enter a html tag of %s page that contains a price of product.\n>' % domain)
                 tag_class = input('Enter class of above tag.\n')
 
                 webpages_list['pages'].append({
@@ -82,13 +85,12 @@ def price_scraper(products_path, supported_pages_path):
                 with open(supported_pages_path, 'w') as f:
                     json.dump(webpages_list, f)
 
-            elif response == 'N' or 'n':
+            elif str.lower(response) == 'n':
                 exit()
 
             else:
                 print('Unrecognized text typed.')
                 exit()
-
 
         try:
             response = requests.get(url)
@@ -107,11 +109,18 @@ def price_scraper(products_path, supported_pages_path):
             continue
 
         actual_price = Price.fromstring(price_text)
+        price_amount = actual_price.amount
+        # price_currency = actual_price.currency
 
-        print(domain)
-        print(actual_price)
+        if price_amount <= price:
+            msg = 'Price of a product under this address: {} has dropped to {}.'.format(url, str(price_amount))
+            send_mail(body=msg)
 
-        #if actual_price <= price:
+
 
 if __name__ == "__main__":
-    price_scraper('products.json', 'supported_pages.json')
+
+    while True:
+        price_scraper('products.json', 'supported_pages.json')
+        print('{}\nProgram is sleeping. Next price checking in {} minutes.'.format(datetime.now(), float(SEC_INTERVAL) / 60))
+        sleep(SEC_INTERVAL)
